351ce27982390992f3d575f1b116bd4f89da8dfd
Mang-Joo <wjwan0915@gmail.com>
2025-01-22 12:59:47 +0900

    ✨ feat: Add Init Redis Session

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use std::{env, sync::Arc};
 
 use axum::{Extension, Router};
-use config::{app_state::AppState, db::init_db, jwt::JwtManager};
+use config::{
+    app_state::AppState,
+    db::{init_db, init_redis_session_store},
+    jwt::JwtManager,
+};
 use tokio::net::TcpListener;
 use user::create_user_router;
 
@@ -30,8 +34,10 @@ pub async fn start_server() {
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let jwt_manager = JwtManager::new(secure.as_bytes());
     let db_pool = init_db(db_url).await;
+    let session_store =
+        init_redis_session_store(env::var("REDIS_URL").expect("REDIS URL must be set"));
 
-    let app_state = Arc::new(AppState::new(db_pool));
+    let app_state = Arc::new(AppState::new(db_pool, session_store));
 
     let chat_router = chat::create_chat_router().await;
     let user_router = create_user_router(Arc::clone(&app_state)).await;
