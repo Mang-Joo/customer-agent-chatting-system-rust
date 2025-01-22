fd7c90c000eda8c260fbe63bbeb6c0154ecd290f
Mang-Joo <wjwan0915@gmail.com>
2025-01-22 13:41:24 +0900

    ✨ feat: Add Register API for agent
    Speration of user and agent Register APIs

--- src/user/handler.rs
+++ src/user/handler.rs
@@ -1,20 +1,44 @@
 use axum::{Extension, Json};
 use serde::Deserialize;
 
-use crate::config::{error::AppError, jwt::JwtManager};
+use crate::config::{error::AppError, jwt::JwtManager, MangJooResult};
 
-use super::service::{UserLogin, UserRegister, UserService};
+use super::{
+    service::{UserLogin, UserRegister, UserService},
+    user::UserRole,
+};
 
 #[tracing::instrument]
 pub async fn register_user(
     Extension(user_service): Extension<UserService>,
     Extension(jwt_manager): Extension<JwtManager>,
     Json(request): Json<RegisterUserRequest>,
-) -> Result<Json<String>, AppError> {
-    let user_register = UserRegister::new(request.email, request.password, request.name);
+) -> MangJooResult<Json<String>> {
+    let user_register = UserRegister::new(
+        request.email,
+        request.password,
+        request.name,
+        UserRole::User,
+    );
     let token = user_service.register(user_register, &jwt_manager).await?;
 
     Ok(Json(String::from(token)))
+}
+
+#[tracing::instrument]
+pub async fn register_agent(
+    Extension(user_service): Extension<UserService>,
+    Extension(jwt_manager): Extension<JwtManager>,
+    Json(request): Json<RegisterUserRequest>,
+) -> MangJooResult<Json<String>> {
+    let user_register = UserRegister::new(
+        request.email,
+        request.password,
+        request.name,
+        UserRole::Agent,
+    );
+    let token = user_service.register(user_register, &jwt_manager).await?;
+    Ok(Json(token))
 }
 
 pub async fn login_hander(
